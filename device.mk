703a779c685187ff8ff153a2e70f2ccdfad4fe64
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2020-03-03 08:58:27 -0500

    Update device.mk
    change vendor name

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_COPY_FILES += \
     device/motorola/qcom-common/idc/atmxt-i2c.idc:system/usr/idc/atmxt-i2c.idc \
     device/motorola/qcom-common/idc/evfwd.idc:system/usr/idc/evfwd.idc \
     device/motorola/qcom-common/idc/atmxt-i2c.idc:recovery/root/vendor/firmware/atmxt-i2c.idc \
-    vendor/motorola/xt907_jbbl/proprietary/etc/firmware/atmxt-r2.tdat:recovery/root/vendor/firmware/atmxt-r2.tdat
+    vendor/motorola/mb886/proprietary/etc/firmware/atmxt-r2.tdat:recovery/root/vendor/firmware/atmxt-r2.tdat
  
 # QCOM Display
 PRODUCT_PROPERTY_OVERRIDES += \
